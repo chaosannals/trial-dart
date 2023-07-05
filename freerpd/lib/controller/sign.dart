e63209469a7cffd48454c9644f04ade933351714
chenshenchao <chenshenchao@outlook.com>
2023-07-05 21:39:44 +0800

    save

---
+++ freerpd/lib/controller/sign.dart
@@ -0,0 +1,6 @@
+import '../request.dart';
+
+@Request("")
+class SignController {
+  
+}
---
+++ freerpd/test/freerpd_test.dart
@@ -0,0 +1,8 @@
+import 'package:freerpd/freerpd.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(respond({ 'a': 'b' }), 42);
+  });
+}
